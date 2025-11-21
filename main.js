adfd43f1c4810749ccb8f8055802745dfcf1b3fb
mohirjongofurjonov <mohirjongofurjonov@gmail.com>
2025-11-21 06:59:50 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,53 @@
+// let numbers = [10, 65, 30, 80, 5, 99];
+
+// let otvet = [];
+
+// for (let i = 0; i < numbers.length; i++) {
+//     if (numbers[i] > 50) {
+//         otvet.push(numbers[i]);
+//     }
+// }
+
+// console.log(otvet); 
+
+
+// let nums = [10, 20, 30, 40];
+
+// let suma = 0;
+
+// for (let i = 0; i < nums.length; i++) {
+//     suma+ nums[i];
+// }
+
+// console.log("Сумма:", suma); 
+
+
+// let rost = prompt("Введите возраст:");
+
+// rost = Number(rost);
+
+// if (rost < 18) {
+//     console.log("Несовершеннолетний");
+// } else if (rost <= 60) {
+//     console.log("Взрослый");
+// } else {
+//     console.log("Пожилой человек");
+// }
+
+
+// let Chisla = [5, -3, 8, 0, -9, 12, -1];
+
+// let positive = [];
+// let negative = [];
+
+// for (let i = 0; i < Chisla.length; i++) {
+//     if (Chisla[i] >= 0) {
+//         positive.push(Chisla[i]);
+//     } else {
+//         negative.push(Chisla[i]);
+//     }
+// }
+
+// console.log("Положительные:", positive);
+// console.log("Отрицательные:", negative);
+
